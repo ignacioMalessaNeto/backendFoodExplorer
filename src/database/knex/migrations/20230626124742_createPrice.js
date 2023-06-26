ce061b2dcb797ef7fb32801842141ed38b38fb93
Ignacio <projetosignacio@gmail.com>
2023-06-26 10:17:13 -0300

    Create migration to prices and tags

---
+++ src/database/knex/migrations/20230626124742_createPrice.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("prices", table => {
+    table.increments("id");
+    table.text("name");
+    table.text("description");
+    table.text("img_price").nullable();
+    table.enum("category",["meal", "dessert", "drinks"]);
+    table.integer("price");
+    table.integer("adm_id").references("id").inTable("users");
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable("prices");
